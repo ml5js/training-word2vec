7c991c5cdc8b340ec028ecfa9f110359022568a5
Fernando Aguilar <fernando@aguilar.net.br>
2018-10-29 20:00:00 -0300

    Add Input with Single File or Folder Functionality

--- train.py
+++ train.py
@@ -6,8 +6,9 @@ import json
 import sys
 import argparse
 import glob
+import os
 
-#Parsing for the user arguments 
+#Parsing for the user arguments
 parser = argparse.ArgumentParser(description="Text File to Word2Vec Vectors")
 
 #Required input file
@@ -19,14 +20,19 @@ parser.add_argument("-o", "--output", default="vector.json", help="Path to the o
 args = parser.parse_args()
 
 #Using the arguments from the arg dictionary
-# text = open(args.input).read().lower().replace("\n", " ")
 output_text_file = args.output
 
-listOfFiles = glob.glob(args.input + '/*.txt')
+listOfFiles = []
+if os.path.isdir(args.input):
+    # Make a list with all txt in the folder
+    listOfFiles = glob.glob(args.input + '/*.txt')
+else:
+    # use a single file
+    listOfFiles.append(args.input)
 
 final_sentences = []
 for file in listOfFiles:
-    text = open(file).read().lower().replace("\n", " ")
+    text = open(file).read().lower().replace("\n", " ") # Remove lineabreaks
     # Split into sentences (this could be improved! Using nltk?)
     sentences = re.split("[.?!]", text)
     # Split each sentence into words! (this could also be improved!)
@@ -37,7 +43,7 @@ for file in listOfFiles:
 
 # Create the Word2Vec model
 model = Word2Vec(final_sentences, size=100, window=5, min_count=5, workers=4)
-# Save the vectors to a text file 
+# Save the vectors to a text file
 model.wv.save_word2vec_format(output_text_file, binary=False)
 
 # Open up that text file and convert to JSON
